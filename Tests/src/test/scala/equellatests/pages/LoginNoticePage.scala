267a56997229bbfd2abc0bc5201515afc46d87a9
Samantha Fisher <samantha.fisher@edalex.com>
2019-01-24 16:50:59 +1100

    Add Autotest for create HTML Notice #720
    Also add a new Page type for new UI pages.

---
+++ Tests/src/test/scala/equellatests/pages/LoginNoticePage.scala
@@ -0,0 +1,24 @@
+package equellatests.pages
+
+import com.tle.webtests.framework.PageContext
+import equellatests.browserpage.NewTitledPage
+import org.openqa.selenium.{Keys, WebElement}
+
+case class LoginNoticePage(ctx: PageContext) extends NewTitledPage("Login Notice Editor", "page/loginconfiguration") {
+
+  def applyButton:WebElement = findElementById("applyButton")
+
+  def noticeField:WebElement = findElementById("noticeField")
+
+  def clickApply(): Unit = {
+    this.applyButton.click()
+  }
+  def populateNoticeField(notice:String): Unit = {
+    noticeField.sendKeys(Keys.chord(Keys.CONTROL, "a"))
+    noticeField.sendKeys(Keys.DELETE)
+    noticeField.sendKeys(notice)
+  }
+  def getNoticeField:String = {
+    noticeField.getText
+  }
+}
---
+++ Tests/src/test/scala/equellatests/tests/LoginNoticeMenuProperties.scala
@@ -0,0 +1,20 @@
+package equellatests.tests
+
+import equellatests.ShotProperties
+import equellatests.instgen.fiveo.autoTestLogon
+import equellatests.pages.LoginNoticePage
+import org.scalacheck.Prop
+import org.scalacheck.Prop.forAll
+
+object LoginNoticeMenuProperties extends ShotProperties ("Login Notice Menu Properties"){
+  property("plaintext notice creation") = forAll { (w1: String) =>
+    withLogon(autoTestLogon) { context =>
+      val page = LoginNoticePage(context).load()
+      val notice = s"${w1}"
+      page.populateNoticeField(notice)
+      page.clickApply()
+      page.load()
+      Prop(page.getNoticeField==notice).label("Notice: " + notice + ", NoticeField: " + page.getNoticeField)
+    }
+  }
+}
