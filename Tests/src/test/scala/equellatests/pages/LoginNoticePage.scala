7873e4269599e92911a8c2ae02b639b5cd627a8d
Samantha Fisher <samantha.fisher@edalex.com>
2019-01-31 16:42:18 +1100

    Add tests
    Test creation and deletion of login notices of both types, and also
    the existence or lack thereof of the notice element on the login page.

--- Tests/src/test/scala/equellatests/pages/LoginNoticePage.scala
+++ Tests/src/test/scala/equellatests/pages/LoginNoticePage.scala
@@ -6,22 +6,52 @@ import org.openqa.selenium.{Keys, WebElement}
 
 case class LoginNoticePage(ctx: PageContext) extends NewTitledPage("Login Notice Editor", "page/loginconfiguration") {
 
-  def applyButton:WebElement = findElementById("applyButton")
+  def preNoticeApplyButton:WebElement = findElementById("preApplyButton")
 
-  def noticeField:WebElement = findElementById("noticeField")
+  def preNoticeDeleteButton:WebElement = findElementById("preDeleteButton")
 
-  def clickApply(): Unit = {
-    this.applyButton.click()
+  def preNoticeField:WebElement = findElementById("preNoticeField")
+
+  def postNoticeApplyButton:WebElement = findElementById("postApplyButton")
+
+  def postNoticeDeleteButton:WebElement = findElementById("postDeleteButton")
+
+  def postNoticeField:WebElement = findElementById("postNoticeField")
+  
+  def clickPreApply(): Unit = {
+    this.preNoticeApplyButton.click()
   }
 
-  def populateNoticeField(notice:String): Unit = {
-    noticeField.sendKeys(Keys.chord(Keys.CONTROL, "a"))
-    noticeField.sendKeys(Keys.DELETE)
-    noticeField.sendKeys(notice)
+  def clickPreDelete(): Unit = {
+    this.preNoticeDeleteButton.click()
   }
 
-  def getNoticeFieldContents:String = {
-    noticeField.getText
+  def populatePreNoticeField(notice:String): Unit = {
+
+    preNoticeField.sendKeys(Keys.chord(Keys.CONTROL, "a"))
+    preNoticeField.sendKeys(Keys.DELETE)
+    preNoticeField.sendKeys(notice)
   }
 
+  def getPreNoticeFieldContents:String = {
+    preNoticeField.getText
+  }
+  
+  def clickPostApply(): Unit = {
+    this.postNoticeApplyButton.click()
+  }
+
+  def clickPostDelete(): Unit = {
+    this.postNoticeDeleteButton.click()
+  }
+
+  def populatePostNoticeField(notice:String): Unit = {
+    postNoticeField.sendKeys(Keys.chord(Keys.CONTROL, "a"))
+    postNoticeField.sendKeys(Keys.DELETE)
+    postNoticeField.sendKeys(notice)
+  }
+
+  def getPostNoticeFieldContents:String = {
+    postNoticeField.getText
+  }
 }
--- Tests/src/test/scala/equellatests/tests/LoginNoticeMenuProperties.scala
+++ Tests/src/test/scala/equellatests/tests/LoginNoticeMenuProperties.scala
@@ -1,21 +1,86 @@
 package equellatests.tests
 
 import equellatests.ShotProperties
+import equellatests.domain.RandomWord
 import equellatests.instgen.fiveo.autoTestLogon
-import equellatests.pages.LoginNoticePage
+import equellatests.pages.{LoginNoticePage, LoginPage}
+import org.openqa.selenium.By
 import org.scalacheck.Prop
 import org.scalacheck.Prop.forAll
 
-object LoginNoticeMenuProperties extends ShotProperties ("Login Notice Menu Properties"){
+object LoginNoticeMenuProperties extends ShotProperties("Login Notice Menu Properties") {
 
-  property("plaintext notice creation") = forAll { w1: String =>
+  property("pre login notice creation") = forAll { w1: RandomWord =>
     withLogon(autoTestLogon) { context =>
       val page = LoginNoticePage(context).load()
-      val notice = s"${w1}"
-      page.populateNoticeField(notice)
-      page.clickApply()
+      val notice = s"${w1.word}"
+      page.populatePreNoticeField(notice)
+      page.clickPreApply()
       page.load()
-      Prop(page.getNoticeFieldContents==notice).label("Notice: " + notice + ", NoticeField: " + page.getNoticeFieldContents)
+      Prop(page.getPreNoticeFieldContents == notice).label("Notice: " + notice + ", NoticeField: " + page.getPreNoticeFieldContents)
+    }
+  }
+
+  property("post login notice creation") = forAll { w1: RandomWord =>
+    withLogon(autoTestLogon) { context =>
+      val page = LoginNoticePage(context).load()
+      val notice = s"${w1.word}"
+      page.populatePostNoticeField(notice)
+      page.clickPostApply()
+      page.load()
+      Prop(page.getPostNoticeFieldContents == notice).label("Notice: " + notice + ", NoticeField: " + page.getPostNoticeFieldContents)
+    }
+  }
+
+  property("pre login notice deletion") = forAll { w1: RandomWord =>
+    withLogon(autoTestLogon) { context =>
+      val page = LoginNoticePage(context).load()
+      val notice = s"${w1.word}"
+      page.populatePreNoticeField(notice)
+      page.clickPreApply()
+      page.load()
+      page.clickPreDelete()
+      page.load()
+      Prop(page.getPreNoticeFieldContents == "")
+    }
+  }
+
+  property("post login notice deletion") = forAll { w1: RandomWord =>
+    withLogon(autoTestLogon) { context =>
+      val page = LoginNoticePage(context).load()
+      val notice = s"${w1.word}"
+      page.populatePostNoticeField(notice)
+      page.clickPostApply()
+      page.load()
+      page.clickPostDelete()
+      page.load()
+      Prop(page.getPostNoticeFieldContents == "")
+    }
+  }
+
+  property("prove existence on login page after creation") = forAll { w1: RandomWord =>
+    withLogon(autoTestLogon) { context =>
+      val page = LoginNoticePage(context).load()
+      val notice = s"${w1.word}"
+      page.populatePreNoticeField(notice)
+      page.clickPreApply()
+      page.load()
+      val page2 = LoginPage(context).load()
+      Prop(page2.findElementO(By.id("loginNotice")).get.getText == notice)
+    }
+  }
+
+  property("prove non-existence on login page after deletion") = forAll { w1: RandomWord =>
+    withLogon(autoTestLogon) { context =>
+      val page = LoginNoticePage(context).load()
+      val notice = s"${w1.word}"
+      page.populatePreNoticeField(notice)
+      page.clickPreApply()
+      page.load()
+      page.clickPreDelete()
+      page.load()
+      val page2 = LoginPage(context).load()
+      Prop(page2.findElementO(By.id("loginNotice")).isEmpty)
     }
   }
 
