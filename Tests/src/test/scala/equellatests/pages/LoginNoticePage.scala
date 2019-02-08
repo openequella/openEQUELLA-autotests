9bdd2351a45ce64ee86c21596c4acf4ca1c1f6da
Samantha Fisher <samantha.fisher@edalex.com>
2019-02-08 11:00:59 +1100

    Refactor tests to account for tabs and clear confirmation

--- Tests/src/test/scala/equellatests/pages/LoginNoticePage.scala
+++ Tests/src/test/scala/equellatests/pages/LoginNoticePage.scala
@@ -8,22 +8,29 @@ case class LoginNoticePage(ctx: PageContext) extends NewTitledPage("Login Notice
 
   def preNoticeApplyButton:WebElement = findElementById("preApplyButton")
 
-  def preNoticeDeleteButton:WebElement = findElementById("preDeleteButton")
+  def preNoticeClearButton:WebElement = findElementById("preClearButton")
 
   def preNoticeField:WebElement = findElementById("preNoticeField")
 
   def postNoticeApplyButton:WebElement = findElementById("postApplyButton")
 
-  def postNoticeDeleteButton:WebElement = findElementById("postDeleteButton")
+  def postNoticeClearButton:WebElement = findElementById("postClearButton")
 
   def postNoticeField:WebElement = findElementById("postNoticeField")
-  
+
+  def preTab:WebElement = findElementById("preTab")
+
+  def postTab:WebElement = findElementById("postTab")
+
+  def clearOkButton:WebElement = findElementById("okToClear")
+
   def clickPreApply(): Unit = {
     this.preNoticeApplyButton.click()
   }
 
-  def clickPreDelete(): Unit = {
-    this.preNoticeDeleteButton.click()
+  def clickPreClear(): Unit = {
+    this.preNoticeClearButton.click()
+    this.clearOkButton.click()
   }
 
   def populatePreNoticeField(notice:String): Unit = {
@@ -41,8 +48,17 @@ case class LoginNoticePage(ctx: PageContext) extends NewTitledPage("Login Notice
     this.postNoticeApplyButton.click()
   }
 
-  def clickPostDelete(): Unit = {
-    this.postNoticeDeleteButton.click()
+  def clickPostClear(): Unit = {
+    this.postNoticeClearButton.click()
+    this.clearOkButton.click()
+  }
+
+  def clickPreTab(): Unit = {
+    this.preTab.click()
+  }
+
+  def clickPostTab(): Unit = {
+    this.postTab.click()
   }
 
   def populatePostNoticeField(notice:String): Unit = {
@@ -54,4 +70,5 @@ case class LoginNoticePage(ctx: PageContext) extends NewTitledPage("Login Notice
   def getPostNoticeFieldContents:String = {
     postNoticeField.getText
   }
+
 }
--- Tests/src/test/scala/equellatests/tests/LoginNoticeMenuProperties.scala
+++ Tests/src/test/scala/equellatests/tests/LoginNoticeMenuProperties.scala
@@ -24,36 +24,40 @@ object LoginNoticeMenuProperties extends ShotProperties("Login Notice Menu Prope
   property("post login notice creation") = forAll { w1: RandomWord =>
     withLogon(autoTestLogon) { context =>
       val page = LoginNoticePage(context).load()
+      page.clickPostTab()
       val notice = s"${w1.word}"
       page.populatePostNoticeField(notice)
       page.clickPostApply()
       page.load()
+      page.clickPostTab()
       Prop(page.getPostNoticeFieldContents == notice).label("Notice: " + notice + ", NoticeField: " + page.getPostNoticeFieldContents)
     }
   }
 
-  property("pre login notice deletion") = forAll { w1: RandomWord =>
+  property("pre login notice clear") = forAll { w1: RandomWord =>
     withLogon(autoTestLogon) { context =>
       val page = LoginNoticePage(context).load()
       val notice = s"${w1.word}"
       page.populatePreNoticeField(notice)
       page.clickPreApply()
-      page.load()
-      page.clickPreDelete()
+      page.clickPreClear()
       page.load()
       Prop(page.getPreNoticeFieldContents == "")
     }
   }
 
-  property("post login notice deletion") = forAll { w1: RandomWord =>
+  property("post login notice clear") = forAll { w1: RandomWord =>
     withLogon(autoTestLogon) { context =>
       val page = LoginNoticePage(context).load()
       val notice = s"${w1.word}"
+      page.clickPostTab()
       page.populatePostNoticeField(notice)
       page.clickPostApply()
       page.load()
-      page.clickPostDelete()
+      page.clickPostTab()
+      page.clickPostClear()
       page.load()
+      page.clickPostTab()
       Prop(page.getPostNoticeFieldContents == "")
     }
   }
@@ -70,18 +74,17 @@ object LoginNoticeMenuProperties extends ShotProperties("Login Notice Menu Prope
     }
   }
 
-  property("prove non-existence on login page after deletion") = forAll { w1: RandomWord =>
+  property("prove non-existence on login page after clear") = forAll { w1: RandomWord =>
     withLogon(autoTestLogon) { context =>
       val page = LoginNoticePage(context).load()
       val notice = s"${w1.word}"
       page.populatePreNoticeField(notice)
       page.clickPreApply()
       page.load()
-      page.clickPreDelete()
+      page.clickPreClear()
       page.load()
       val page2 = LoginPage(context).load()
       Prop(page2.findElementO(By.id("loginNotice")).isEmpty)
     }
   }
-
 }
